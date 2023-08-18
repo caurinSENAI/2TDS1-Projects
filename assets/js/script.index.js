311591c93b782f03c5e4c4a9bd83534ed08b4cff
caurin <gabrielcaurin.564@gmail.com>
2023-08-18 09:31:14 -0300

    feat: create category-pruducts files

---
+++ assets/js/script.index.js
@@ -0,0 +1,84 @@
+/* const names = ["Caurin", "Pravel", "Massudo", "Manu"];
+
+names.forEach((name)=>{
+    console.log(`Hi ${name}`)
+}); */
+
+
+
+
+
+/* const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers);
+numbers.forEach((number, index)=>{
+    if (number % 2 == 0){
+        console.log(`O numero ${number} está na posição ${index}`)    
+    }
+}); */
+
+
+
+/* class Car{
+    constructor(param1, param2){
+    this.brand = param1;
+    this.model = param2;
+    }
+}
+
+class CarsList{
+    constructor(){
+        this.cars = [];
+    }
+
+    addCar(param){
+    this.cars.push(param)
+    }
+}
+
+const list = new CarsList(); 
+
+function createCar() {
+    let brand = document.getElementById("carro").value;
+    let model = document.getElementById("modelo").value; */
+
+
+/* console.log(brand, model); */
+
+/* const hotwheels = new Car(brand, model);
+
+list.addCar(hotwheels);
+
+console.log(hotwheels);
+
+list.cars.forEach(car =>{
+    console.log(`Marca:${car.brand} Modelo: ${car.model}`)
+
+})
+} */
+
+
+
+/* const cars = [
+    {
+    jogo: "Fortnite",
+    empresa: "Epic jogos"    
+},
+{
+    jogo: "Valorant",
+    empresa: "Riot"
+},
+{
+    jogo: "Minecraft",
+    empresa: "Mojang"
+},
+{
+    jogo: "Clash Royal",
+    empresa: "Supercell"
+},   
+] */
+
+/* cars.forEach(car =>{
+    console.log(`jogo:${car.jogo} - empresa: ${car.empresa}`)
+
+}); */
